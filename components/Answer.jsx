94244d7dc0dc0f51fe55cd1885986661595e1494
aymane <aymensarrar1@gmail.com>
2022-04-14 05:13:49 +0000

    adding react context api to pass props between parents and childrens

--- components/Answer.jsx
+++ components/Answer.jsx
@@ -1,19 +1,19 @@
 import {Heading} from '@chakra-ui/react'
-import { useState } from 'react';
-
+import { useState, useContext, useEffect } from 'react';
+import { gameContext } from './utils/gameContext';
 const Answer = ({answer, cap}) => {
-	const [color, setColor] = useState('#6066D0CC');
-	const [bgcolor, setBgcolor] = useState('#ffffff');
+	const {data, isCorrect, setIsCorrect} = useContext(gameContext);
+	const [correct, setCorrect] = useState(0)
+
 	const handleClick = e => {
-			e.preventDefault();
-			if (e.target.innerText === cap);
-			{
-				setBgcolor("#60BF88");
-				setColor("#ffffff");
-			}
+		e.preventDefault();
+		if (e.target.innerText === cap)
+			setCorrect(1);
+		else
+			setCorrect(2);
 	}
 	return (
-		<Heading onClick={handleClick} fontWeight="500" color={color} mb="20px" width="80%" borderRadius="12px" borderColor="#6066D0CC" borderWidth="2px" padding="1rem" cursor="pointer" _hover={{color: "white", backgroundColor: "#F9A826", borderColor: "#F9A826"}} fontSize="xl" backgroundColor={bgcolor} as="h3">{answer}</Heading>
+		<Heading onClick={handleClick} fontWeight="500" color={correct == 0 ? "#6066D0CC" : "#ffffff"} mb="20px" width="80%" borderRadius="12px" borderColor={correct == 0 ? "#6066D0CC" : (correct == 1 ? "#60BF88" : "#EA8282")} borderWidth="2px" padding="1rem" cursor={correct == 0 ? "pointer" : "default"} _hover={correct == 0 ? {color: "white", backgroundColor: "#F9A826", borderColor: "#F9A826"} : {}} fontSize="xl" backgroundColor={correct == 0 ? "#ffffff" : (correct == 1 ? "#60BF88" : "#EA8282")} as="h3">{answer}</Heading>
 	)
 }
 export {Answer};
